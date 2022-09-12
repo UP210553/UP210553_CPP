203ca843cb5177907682543bfefa012679a8ab41
UP210553 <up210553@alumnos.upa.edu.mx>
2022-09-12 10:50:27 -0500

    trabajo 1

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,21 @@
+/*
+Date: 12/09/2022
+Author: Ángel de Jesús Martínez Guevara
+Email: up210553@alumnos.upa.edu.mx
+Description: Primer trabajo programación
+Last modification:
+*/
+//-----------PREPOCESSOR DIRECTIVES--------------//
+
+//Input/output library between the screen n' keyboard
+#include <iostream>
+
+//------------MAIN FUNCTION----------------//
+int main(){
+    //Print on screen a text
+    std::cout<<"Hello World!" ;
+
+    // Return 0 means that the programm ends succesfully
+    return 0;
+
+}
