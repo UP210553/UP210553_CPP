89e3ceaf5b5dc85fea808fa7fccd8e403ee6be64
UP210553 <up210553@alumnos.upa.edu.mx>
2022-10-05 10:23:15 -0500

    update

--- U2/05_Temperatura.cpp
+++ U2/05_Temperatura.cpp
@@ -10,27 +10,29 @@ Last modification: 03/10/2022
 #include <iostream>
 using namespace std;
 
-int main(){
-    int contador=0;
-    float tempAcum=0, temperatura, tempMayor=-273, tempMenor=100000;
+int main()
+{
+    int contador = 0;
+    float tempAcum = 0, temperatura, tempMayor = -273, tempMenor = 100000;
 
-    do{
-        cout<<"Give me the temperature: ";
-        cin>>temperatura;
+    do
+    {
+        cout << "Give me the temperature: ";
+        cin >> temperatura;
         tempAcum += temperatura;
         contador++;
-        if(temperatura>=tempMayor){
+        if (temperatura >= tempMayor)
+        {
             tempMayor = temperatura;
         }
-        if(temperatura<=tempMenor){
+        if (temperatura <= tempMenor)
+        {
             tempMenor = temperatura;
         }
-    }while(contador<6);
-    cout<<"The average of temperature is: "<< tempAcum/6<<"°C"<<endl;
-    cout<<"The lower temperature is: "<<tempMenor<<"°C"<<endl;
-    cout<<"The highest temperature is: "<<tempMayor<<"°C"<<endl;
-
+    } while (contador < 6);
+    cout << "The average of temperature is: " << tempAcum / 6 << "°C" << endl;
+    cout << "The lower temperature is: " << tempMenor << "°C" << endl;
+    cout << "The highest temperature is: " << tempMayor << "°C" << endl;
 
     return 0;
-
 }
---
+++ U2/06_Facturas.cpp
@@ -0,0 +1,32 @@
+/*
+Date: 05/10/2022
+Author: Ángel de Jesús Martínez Guevara
+Email: up210553@alumnos.upa.edu.mx
+Description:
+Hacer un programa en el que se ingresen 6 temperaturas y determine el promedio, la mas
+baja y la mas alta
+Last modification: 05/10/2022
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int cantidadDeProductos, contador = 0;
+    float precioDeProducto, totalFactura=0;
+    do
+    {
+        cout << "How many products were sold: ";
+        cin >> cantidadDeProductos;
+        if (cantidadDeProductos != 0)
+        {
+            cout << "Introduce the price of the product: $";
+            cin >> precioDeProducto;
+    
+        }
+        totalFactura += (cantidadDeProductos*precioDeProducto);
+    } while (cantidadDeProductos > 0);
+    cout << "Processing the payment...\n Total cost= $" << totalFactura << endl;
+
+    return 0;
+}
