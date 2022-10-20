0d1b7919a5f9873c999751d2f6d4a50ea9e3a038
UP210553 <up210553@alumnos.upa.edu.mx>
2022-10-20 10:28:55 -0500

    update

--- U3/Funciones.cpp
+++ U3/Funciones.cpp
@@ -10,17 +10,22 @@ Last modification: 20/10/2022
 
 using namespace std;
 int getmaxval(int num1, int num2);
+int getminval(int num1, int num2);
 int main(){
-    int valor1, valor2, valorMax;
+    int valor1, valor2, valorMax, valorMin;
     cout<<"Entry the first valor: ";
     cin>>valor1;
     cout<<"Entry the second valor: ";
     cin>>valor2;
     valorMax = getmaxval(valor1, valor2);
+    valorMin = getminval(valor1, valor2);
     cout<<"The max value is: "<< valorMax << endl;
-    
+    cout<<"The min value is: "<< valorMin << endl;
 return 0;
 }
 int getmaxval(int num1, int num2){
     return (num1 > num2)?num1:num2;
+}
+int getminval(int num1, int num2){
+    return (num1 < num2)?num1:num2;
 }
