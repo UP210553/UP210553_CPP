f7dfbb7761933a2d985e238d04ece0a994a617d5
UP210553 <up210553@alumnos.upa.edu.mx>
2022-10-20 10:24:10 -0500

    update

---
+++ U3/Funciones.cpp
@@ -0,0 +1,26 @@
+/*
+Date: 20/10/2022
+Author: Ángel de Jesús Martínez Guevara
+Email: up210553@alumnos.upa.edu.mx
+Description: Creación y manejo de funciones
+Last modification: 20/10/2022
+*/
+
+#include <iostream>
+
+using namespace std;
+int getmaxval(int num1, int num2);
+int main(){
+    int valor1, valor2, valorMax;
+    cout<<"Entry the first valor: ";
+    cin>>valor1;
+    cout<<"Entry the second valor: ";
+    cin>>valor2;
+    valorMax = getmaxval(valor1, valor2);
+    cout<<"The max value is: "<< valorMax << endl;
+    
+return 0;
+}
+int getmaxval(int num1, int num2){
+    return (num1 > num2)?num1:num2;
+}
