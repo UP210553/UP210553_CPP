f517da0edd25cb07dfd348500e99282c2fc29c8e
UP210553 <up210553@alumnos.upa.edu.mx>
2022-10-26 10:39:03 -0500

    update

--- U3/Cambio.cpp
+++ U3/Cambio.cpp
@@ -3,7 +3,7 @@ Date: 24/10/2022
 Author: Ángel de Jesús Martinez Guevara
 Email: up210553@alumnos.upa.edu.mx
 Description: 
-Last modification: 24/10/2022
+Last modification: 26/10/2022
 */
 
 #include <iostream>
@@ -11,8 +11,7 @@ Last modification: 24/10/2022
 using namespace std;
 
 void obtenerCambio(int);
-void obtenerMonedas(int);
-void imprimirCambio(int);
+void imprimirCambio();
 
 int quinientos = 0, doscientos = 0, cien = 0;
 int cincuenta = 0, veinte = 0;
@@ -20,18 +19,15 @@ int diez = 0, cinco = 0, dos = 0, uno = 0;
 
 int main(){
     int dinero;
+    cout<<"================================================="<<endl;
     cout<<"Give me the amount of money: ";
     cin>>dinero;
     
     obtenerCambio (dinero);
-    imprimirCambio (1);
-
-    obtenerMonedas (dinero);
-    imprimirCambio (0);
+    imprimirCambio ();
 
 return 0;
 }
-
 void obtenerCambio (int monto){
     
     while(monto > 0){
@@ -73,10 +69,16 @@ void obtenerCambio (int monto){
         } 
         }
 }
-void obtenerMonedas(int feria){
-
-}
-void imprimirCambio(int imprimir){
-    (imprimir=1)?cout<<"$500: "<<quinientos<<endl : cout<<"--MONEDAS--"<<endl;
+void imprimirCambio(){
+    cout<<"==========Breakdown========="<<endl;
+    cout<<"$500 bills: \t"<<quinientos<<endl;
+    cout<<"$200 bills: \t"<<doscientos<<endl;
+    cout<<"$100 bills: \t"<<cien<<endl;
+    cout<<"$50 bills: \t"<<cincuenta<<endl;
+    cout<<"$20 bills: \t"<<veinte<<endl;
+    cout<<"$10 coins: \t"<<diez<<endl;
+    cout<<"$5 coins: \t"<<cinco<<endl;
+    cout<<"$2 coins: \t"<<dos<<endl;
+    cout<<"$1 coins: \t"<<uno<<endl;
     
 }
