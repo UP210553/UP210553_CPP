9794c27aa61164b0d4593d47d23596628d8d7f86
UP210553 <up210553@alumnos.upa.edu.mx>
2022-10-30 17:46:45 -0600

    update

---
+++ U3/TableroGato.cpp
@@ -0,0 +1,12 @@
+/*
+Date: 28/10/2022
+Author: Ángel Jesús Martínez Guevara
+Email: up210553@alumnos.upa.edu.mx
+Description: 
+Last modification: 28/10/2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
