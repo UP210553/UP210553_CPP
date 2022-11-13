339a77f8f960cfe6ea9eb4d2425909830e4caded
UP210553 <up210553@alumnos.upa.edu.mx>
2022-11-12 23:47:10 -0600

    update

--- U3/JuegoGato.cpp
+++ U3/JuegoGato.cpp
@@ -2,7 +2,7 @@
 Date: 28/10/2022
 Author: Ángel Jesús Martínez Guevara
 Email: up210553@alumnos.upa.edu.mx
-Description: 
+Description:
 Last modification: 28/10/2022
 */
 
@@ -10,29 +10,31 @@ Last modification: 28/10/2022
 
 using namespace std;
 int jugada, turno = 0, x, y;
-char areaJuego[3][3]={'1','2','3','4','5','6','7','8','9'};
+char areaJuego[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char tableroGato[6][11];
 void generarTableroGato();
 bool comprobarCasillaOcupada(int);
 int seleccionarJugada();
-void colocarJugada(bool);
+void colocarJugada(bool, int);
 bool verificarGanador(int);
-int main(){
 
-    
+int main()
+{
+
     do
-    {   
+    {
         generarTableroGato();
         int j = seleccionarJugada();
         bool test = comprobarCasillaOcupada(j);
-        colocarJugada(test);
+        colocarJugada(test, j);
         turno++;
+        system("cls");
     } while (turno < 10);
-
 
     return 0;
 }
-int seleccionarJugada(){
+int seleccionarJugada()
+{
     do
     {
         cout << "Give me your play: ";
@@ -40,139 +42,287 @@ int seleccionarJugada(){
     } while (jugada < 1 || jugada > 9);
     return jugada;
 }
-bool comprobarCasillaOcupada(int jugada){
+bool comprobarCasillaOcupada(int jugada)
+{
     int x, y;
     if (jugada == 1)
     {
-        x=0;
-        y=0;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 0;
+        y = 0;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
-        
     }
     else if (jugada == 2)
     {
-        x=0;
-        y=1;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 0;
+        y = 1;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else if (jugada == 3)
     {
-        x=0;
-        y=2;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 0;
+        y = 2;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else if (jugada == 4)
     {
-        x=1;
-        y=0;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 1;
+        y = 0;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else if (jugada == 5)
     {
-        x=1;
-        y=1;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 1;
+        y = 1;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else if (jugada == 6)
     {
-        x=1;
-        y=2;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 1;
+        y = 2;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else if (jugada == 7)
     {
-        x=2;
-        y=0;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 2;
+        y = 0;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else if (jugada == 8)
     {
-        x=2;
-        y=1;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 2;
+        y = 1;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
     else
     {
-        x=2;
-        y=2;
-        if (areaJuego[x][y]=='X'||areaJuego[x][y]=='O')
+        x = 2;
+        y = 2;
+        if (areaJuego[x][y] == 'X' || areaJuego[x][y] == 'O')
         {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 }
-void colocarJugada(bool j){
-    if (j==0){
-        if (turno %2 == 0)
+void colocarJugada(bool j, int jugada)
+{
+    int x, y;
+    if (jugada == 1)
+    {
+        x = 0;
+        y = 0;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
             {
-                areaJuego[x][y]='X';
-                turno++;
+                areaJuego[x][y] = 'X';
             }
-            else{
-                areaJuego[x][y]='O';
-                turno++;
+            else
+            {
+                areaJuego[x][y] = 'O';
             }
+        }
+    }
+    else if (jugada == 2)
+    {
+        x = 0;
+        y = 1;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else if (jugada == 3)
+    {
+        x = 0;
+        y = 2;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else if (jugada == 4)
+    {
+        x = 1;
+        y = 0;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else if (jugada == 5)
+    {
+        x = 1;
+        y = 1;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else if (jugada == 6)
+    {
+        x = 1;
+        y = 2;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else if (jugada == 7)
+    {
+        x = 2;
+        y = 0;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else if (jugada == 8)
+    {
+        x = 2;
+        y = 1;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
+    }
+    else
+    {
+        x = 2;
+        y = 2;
+        if (j == 0)
+        {
+            if (turno % 2 == 0)
+            {
+                areaJuego[x][y] = 'X';
+            }
+            else
+            {
+                areaJuego[x][y] = 'O';
+            }
+        }
     }
 }
-void generarTableroGato(){
+void generarTableroGato()
+{
     char tableroGato[6][11];
     for (int x = 0; x < 3; x++)
     {
         for (int y = 0; y < 3; y++)
         {
-            cout<<areaJuego[x][y]<<" ";
+            cout << areaJuego[x][y] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-} 
+}
+bool verificarGanador(int){
+    
+}
