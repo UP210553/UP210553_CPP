5c1eade903476d7c995400a4373e5dc4fa102cfd
Osvaldo <up210188@alumnos.upa.edu.mx>
2022-11-24 10:31:51 -0600

    juego

--- U4/01_BattleShip.cpp
+++ U4/01_BattleShip.cpp
@@ -17,6 +17,7 @@ Description:
 using namespace std;
 void buildBoard();
 void creartablero();
+void imprimirjugadas();
 
 char matriz[10][10];
 //-----------MAIN FUNTION---------
@@ -28,8 +29,6 @@ int main()
 void buildBoard(){
 
     cout<<"holiiiiiiiiiiiiiiiii";
-
-    cout<<"adiosssssssssss";
     
 }
 
@@ -39,7 +38,13 @@ for (int i = 0; i < 10; i++){
         matriz[i][l] = 0;
     }
 }
-cout<< endl;
-
 }
 
+void imprimirjugada(){
+    for (int i = 0; i < 10; i++){
+    for (int l = 0; l < 10; l++){
+        cout << matriz[i][l];
+    }
+    cout << endl;
+}
+}
