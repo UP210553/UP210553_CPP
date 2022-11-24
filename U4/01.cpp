a15cace1b58b880a4406996bba3eec83cbd75aed
UP210894 <up210894@alumnos.upa.edu.mx>
2022-11-24 09:58:35 -0600

    update

--- U4/01.cpp
+++ U4/01.cpp
@@ -1,1 +1,24 @@
-kaka
+/*
+Date: 
+Authors: Enrique Abel Herrera Vargas
+         Angel de Jesús Martínez Guevara
+         Osvaldo Esparza Gutierrez
+Emails: up210894@alumnos.upa.edu.mx
+        up210553@alumnos.upa.edu.mx
+        up210188@alumnos.upa.edu.mx
+Description:  
+-----Problem-----
+
+*/
+
+// Preprocessor directives
+#include <iostream>
+
+// Namespace avoid using std:: on all input and output
+using namespace std;
+
+//-----------MAIN FUNTION---------
+int main()
+{
+    return 0;
+}
